b95eff18759eac5cea418a94a933c06992f728c6
EC2 Default User <ec2-user@ip-172-31-30-202.ap-northeast-1.compute.internal>
2022-08-22 05:12:05 +0000

    [add]new-item-v

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,13 +3,21 @@ class Admin::ItemsController < ApplicationController
 
   def index
     @books = Book.all
-
+  end
   def new
     @item = Item.new
   end
   
   def create
+    @item = Item.new(item.params)
+    @imte.save
     
-
   end
+  
+  private
+
+  def item_params
+    params.require(:iten).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+  
 end
