386315df610d1c7ed989722a10ca8d1bd5b591da
EC2 Default User <ec2-user@ip-172-31-30-202.ap-northeast-1.compute.internal>
2022-08-23 07:28:45 +0000

    admin-customers-show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,5 @@
 class Admin::ItemsController < ApplicationController
 
-
   def index
     @items = Item.page(params[:page])
   end
@@ -10,14 +9,19 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item.params)
-    @imte.save 
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item(@item.id)
+      flash[:notice] = "商品を登録しました"
+    else
+      flash[:notice] = "必要情報を入力してください"
+      render action: :new
+    end
   end
   
   private
 
   def item_params
-    params.require(:iten).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+    params.require(:item).permit(:image_id, :name, :introduction, :price, :is_active)
   end
-
 end
