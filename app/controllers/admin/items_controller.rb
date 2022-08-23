94fbfa5d5c0dc1d8c177e8894bbc1a58a5ddddd6
oreffice624 <108616070+oreffice624@users.noreply.github.com>
2022-08-23 07:54:41 +0000

    addmypage

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,9 +11,23 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item.params)
-    @imte.save 
+    @imte.save
   end
-  
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path(@item)
+      flash[:notice_update] = "ジャンル情報を更新しました！"
+    else
+      redirect_to edit_admin_item_path(@item)
+    end
+  end
+
   private
 
   def item_params
