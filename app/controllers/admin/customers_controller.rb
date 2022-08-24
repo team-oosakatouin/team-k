b119baa181513a08e06b42b05b6772ea9bdaa93a
EC2 Default User <ec2-user@ip-172-31-38-11.ap-northeast-3.compute.internal>
2022-08-24 10:50:01 +0000

    [add]会員・商品詳細、注文履歴一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,4 +7,22 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
   
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_data_path
+    else
+      flash[:customer_updated_error] = "会員情報が正常に保存されませんでした。"
+      redirect_to edit_admin_customer_data_path(@customer)
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :is_enabled)
+  end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,7 @@
 class Public::OrdersController < ApplicationController
+  
+  def index
+    @orders = Order.where(customer_id:current_customer)
+  end
+  
 end
