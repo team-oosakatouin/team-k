a802e99ff2c7f2f58b5d04f0285a8cfea6b3dbbf
EC2 Default User <ec2-user@ip-172-31-30-202.ap-northeast-1.compute.internal>
2022-08-26 03:55:27 +0000

    8-26-a

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,7 @@
 class Public::HomesController < ApplicationController
-  
-  layout "public_application"
 
   def top
-    @new_items = Item.all.order("created_at DESC").first(4)
+    @items = Item.all.order("created_at DESC").first(4)
     @genres = Genre.all
   end
 
