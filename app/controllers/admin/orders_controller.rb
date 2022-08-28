85e0a7fc6d324720a1caac90d25dc0461ea6fc34
EC2 Default User <ec2-user@ip-172-31-35-188.ap-northeast-1.compute.internal>
2022-08-28 07:15:57 +0000

    インデント修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,12 +1,12 @@
 class Admin::OrdersController < ApplicationController
    def index
-    if params[:id] # 会員詳細から来た場合
+    if params[:id]
       @orders = Customer.find(params[:id]).orders.page(params[:page]).per(10)
 
-    elsif request.fullpath.include? "today" # TOP本日受注分から来た場合
+    elsif request.fullpath.include? "today"
       @orders = Order.where(created_at:  Time.zone.now.all_day).page(params[:page]).per(10)
 
-    elsif request.fullpath.include? "yesterday" # TOP本日製作分から来た場合
+    elsif request.fullpath.include? "yesterday"
       @orders = Order.where(created_at: 1.day.ago.all_day).page(params[:page]).per(10)
 
     else
@@ -20,17 +20,17 @@ class Admin::OrdersController < ApplicationController
    end
 
    def update
-  @order = Order.find(params[:id])
-  @order.update(order_params)
-  @order_details = @order.order_details
-  if @order.status == "payment_comfirmation"
-    @order_details.each do|order_detail|
-    order_detail.making_status = "waiting_for_making"
-    order_detail.save
+     @order = Order.find(params[:id])
+     @order.update(order_params)
+     @order_details = @order.order_details
+   if @order.status == "payment_comfirmation"
+     @order_details.each do|order_detail|
+     order_detail.making_status = "waiting_for_making"
+     order_detail.save
    end
-  end
-  redirect_to admin_order_path(@order.id)
-  end
+   end
+     redirect_to admin_order_path(@order.id)
+   end
 
    protected
 
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,20 +1,15 @@
 class Public::ItemsController < ApplicationController
-  
+
    def index
-  	  @genres = Genre.where(is_enabled: true)# ジャンルが有効のみ
-  	  
+  	  @genres = Genre.where(is_enabled: true)
 
-      # もしURLに[:genre_id]が含まれていたら
+
+
     if params[:genre_id]
-      # その[:genre_id]のデータをGenreから@genreに格納
       @genre = Genre.find(params[:genre_id])
-      # @genreに紐付いた商品で販売可の商品を引っ張る（作成順の昇順）
       @items = @genre.items.order(created_at: :desc).where(is_active: "販売可").page(params[:page]).per(8)
-
-      # 含まれていなければ
     else
       @items = Item.where(is_active: "販売可").page(params[:page]).per(8)
-     # 販売ステータスが販売可のみの商品を参照 ジャンル作成の昇順
     end
    end
 
@@ -23,5 +18,5 @@ class Public::ItemsController < ApplicationController
   	@genres = Genre.all
   	@item = Item.find(params[:id])
   end
-  
+
 end
